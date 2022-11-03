2aacd43f159acff0ba8f9992678b4672d263b813
ldydek <91955435+ldydek@users.noreply.github.com>
2022-11-03 13:02:21 +0100

    Delete Ex01 directory

--- Ex01/script.js
+++
@@ -1,5 +0,0 @@
-document.querySelector("#button").addEventListener('click', function() {
-    let imie = prompt("Twoje imię: ");
-    buttonContent = imie;
-    document.querySelector("#section").textContent = buttonContent;
-});
