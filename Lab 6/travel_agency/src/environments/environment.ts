9b3c23ae00bf5d78db4c4c1cdc8309fa477739c3
ldydek <91955435+ldydek@users.noreply.github.com>
2022-12-29 17:57:27 +0100

    Update environment.ts

--- Lab 6/travel_agency/src/environments/environment.ts
+++ Lab 6/travel_agency/src/environments/environment.ts
@@ -1,13 +1,1 @@
-export const environment = {
-    production: true,
-    firebase: {
-        apiKey: "AIzaSyDDI1urW-4w2dYW_wC1QNNettNUmD3CPRA",
-        authDomain: "agencja-turystyczna.firebaseapp.com",
-        databaseURL: "https://agencja-turystyczna-default-rtdb.europe-west1.firebasedatabase.app",
-        projectId: "agencja-turystyczna",
-        storageBucket: "agencja-turystyczna.appspot.com",
-        messagingSenderId: "441218534746",
-        appId: "1:441218534746:web:c319e7914d8748dce3f763",
-        measurementId: "G-BPQED7P9HK"
-    }
-};
+
