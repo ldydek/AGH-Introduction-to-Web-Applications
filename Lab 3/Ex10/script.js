a708af3d51d208577b366a1dfbd8a315215cf6ad
ldydek <lukaszdydek72@gmail.com>
2023-01-22 12:10:19 +0100

    commit

--- Lab 3/Ex10/script.js
+++ Lab 3/Ex10/script.js
@@ -1,24 +1,35 @@
 let board = document.querySelector("#board");
 let outside = document.querySelector("body");
 let info = document.querySelector("#info");
-let p1 = document.querySelector("#p1");
-let p2 = document.querySelector("#p2");
+let warning1 = document.querySelector("#p1");
+let warning2 = document.querySelector("#p2");
 let ball = document.querySelector("#ball");
+let ballWidth = 25;
+let borderThickness = 5;
 
 board.addEventListener("click", move);
-outside.addEventListener("click", outsideFunction);
+outside.addEventListener("click", displayWarning);
 
 function move(event) {
     let x = event.clientX;
     let y = event.clientY;
-    console.log(x);
-    console.log(y);
-    ball.style.left = (x-25) + "px";
-    ball.style.top = (y-25) + "px";
+    let viewPortDimensions = outside.getBoundingClientRect();
+    let boardDimensions = board.getBoundingClientRect();
+
+    let ballHorizontalPosition =  Math.max((x - (viewPortDimensions.width - boardDimensions.width)/2 - ballWidth), 0);
+    ballHorizontalPosition = Math.min(ballHorizontalPosition, boardDimensions.width - 2*(ballWidth + borderThickness));
+
+    let ballVerticalPosition =  Math.max((y - (viewPortDimensions.height - boardDimensions.height)/2 - ballWidth), 0);
+    ballVerticalPosition = Math.min(ballVerticalPosition, boardDimensions.height - 2*(ballWidth + borderThickness));
+
+    ball.style.left =  ballHorizontalPosition + "px";
+    ball.style.top = ballVerticalPosition + "px";
     info.style.display = "none";
     event.stopPropagation();
 }
 
-function outsideFunction() {
+function displayWarning(event) {
     info.style.display = "block";
+    info.style.left = event.clientX + "px";
+    info.style.top = event.clientY + "px";
 }
