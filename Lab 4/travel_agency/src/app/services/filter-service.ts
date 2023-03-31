0912875e3edf2357a038b5deb51bf6fddc26a653
ldydek <91955435+ldydek@users.noreply.github.com>
2023-03-31 16:44:24 +0200

    Add files via upload

---
+++ Lab 4/travel_agency/src/app/services/filter-service.ts
@@ -0,0 +1,17 @@
+import {Injectable} from '@angular/core';
+import { Journey } from "../interfaces/journey"
+
+@Injectable({
+    providedIn: 'root'
+  })
+export class FilterService {
+    journeys: Journey[] = []
+
+    getAllData() {
+      
+    }
+
+    getMinPrice() {
+
+    }
+}
